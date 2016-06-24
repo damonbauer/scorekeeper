8d19ad9d1a6906afac42562801cbfe57da65836c
Damon Bauer <damon.bauer@daveramsey.com>
2016-06-23 19:25:04 -0500

    Update scripts

--- app/config/routes.js
+++ app/config/routes.js
@@ -6,7 +6,7 @@ import GameContainer from '../containers/GameContainer';
 
 export default (
   <Router history={browserHistory}>
-    <Route path='/' component={Main}>
+    <Route path='/scorekeeper' component={Main}>
       <IndexRoute component={PlayersContainer} />
       <Route path='game' component={GameContainer} />
     </Route>
