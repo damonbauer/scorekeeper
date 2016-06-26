dc9afbba5d1d8cd29d76583361814657343be2b8
Damon Bauer <damon.bauer@daveramsey.com>
2016-06-26 14:12:39 -0500

    Sticky header, don't allow form submit if any inputs are empty

--- app/components/Game/Round.js
+++ app/components/Game/Round.js
@@ -9,15 +9,20 @@ export default class Round extends React.Component {
     };
   }
 
+  inputHasValues(element) {
+    return element.value.trim() !== '';
+  }
+
   onFormSubmit(e) {
     e.preventDefault();
 
     let formEls = Array.from(e.target.getElementsByTagName('input'));
     let scores = formEls.map(input => parseInt(input.value, 10));
 
-    formEls.forEach(input => input.setAttribute('disabled', 'disabled'));
-
-    this.props.handleFormSubmit(scores);
+    if (formEls.every(::this.inputHasValues)) {
+      formEls.forEach(input => input.setAttribute('disabled', 'disabled'));
+      this.props.handleFormSubmit(scores);
+    }
   }
 
   render() {
@@ -26,7 +31,7 @@ export default class Round extends React.Component {
         <p className="Game-cell">{this.props.roundNum}</p>
         {this.props.players.map((player, index) =>
           <div className="Game-cell" key={`cell_${this.props.roundNum}_${index}`}>
-            <input type="text" pattern="[-0-9]*" />
+            <input type="number" />
           </div>
         )}
         <button type="submit" id="add-row">
